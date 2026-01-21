900cc08cf2e739a0b5f3c59ad1b63a97981ab033
tuomaspelkone <tuomas@pelkonen.com>
2026-01-21 13:12:01 +0200

    valmis

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+const [ModalVisible, setModalVisible] = useState(false)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Modal
+          animationType="fade"
+          transparent={true}
+          visible={ModalVisible}
+          onRequestClose={() => {
+            setModalVisible(!ModalVisible);
+          }}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text>This is modal...</Text>
+                <Pressable
+                  onPress={() => setModalVisible(!ModalVisible)}>
+                  <Text style={styles.closeModalText}>Close</Text>
+                </Pressable>
+              </View>
+            </View>
+        </Modal>
+      <Pressable onPress={() => setModalVisible(true)}>
+          <Text>Show Modal message</Text>
+        </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +39,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00000099',
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 80,
+    alignItems: 'center',
+  },
+  closeModalText: {
+    marginTop: 20,
+    fontWeight: 'bold',
   },
 });
